6f6aa578a3b13c34372a445667812bb650c68dec
Joseph Delong <joseph@delong.me>
2023-11-17 11:21:32 -0600

    feat: increase decimal tests

--- test/TestCompoundInterest.sol
+++ test/TestCompoundInterest.sol
@@ -17,110 +17,80 @@ contract TestCompoundInterest is AstariaV1Test {
     using FixedPointMathLib for int256;
 
     function testRateTooLowZero() public {
-        defaultPricingData = abi.encode(BasePricing.Details({carryRate: 0, rate: 0, decimals: 18}));
-        Starport.Loan memory loan = generateDefaultLoanTerms();
-        vm.expectRevert(abi.encodeWithSelector(AstariaV1Lib.InterestAccrualRoundingMinimum.selector));
-        lenderEnforcer.validate(new AdditionalTransfer[](0), loan, "");
+        vm.expectRevert(AstariaV1Lib.InterestAccrualRoundingMinimum.selector);
+        AstariaV1Lib.validateCompoundInterest(1e18, 0, 10e18, 18);
     }
 
     function testRateTooLowOne() public {
-        defaultPricingData = abi.encode(BasePricing.Details({carryRate: 0, rate: 1, decimals: 18}));
-        Starport.Loan memory loan = generateDefaultLoanTerms();
-        loan.debt[0].amount = 1;
-        vm.expectRevert(abi.encodeWithSelector(AstariaV1Lib.InterestAccrualRoundingMinimum.selector));
-        lenderEnforcer.validate(new AdditionalTransfer[](0), loan, "");
+        vm.expectRevert(AstariaV1Lib.InterestAccrualRoundingMinimum.selector);
+        AstariaV1Lib.validateCompoundInterest(1e18, 1, 10e18, 18);
     }
 
-    function testExceedMaxRate() public {
-        uint256 MAX_RATE_PLUS_ONE = ((((uint256(type(int256).max.lnWad())) / AstariaV1Lib.MAX_DURATION)) + 1) * 365 days;
-
-        defaultStatusData = abi.encode(
-            BaseRecall.Details({
-                honeymoon: 1 days,
-                recallWindow: 3 days,
-                recallStakeDuration: 30 days,
-                recallMax: MAX_RATE_PLUS_ONE,
-                // 10%, 0.1
-                recallerRewardRatio: uint256(1e16) * 10
-            })
-        );
-
-        defaultPricingData = abi.encode(BasePricing.Details({carryRate: 0, rate: MAX_RATE_PLUS_ONE, decimals: 18}));
-
-        Starport.Loan memory loan = generateDefaultLoanTerms();
-        loan.debt[0].amount = 2;
-
-        bytes memory caveatData = _generateSignedCaveatLender(loan, lender, 0, true).caveats[0].data;
-        vm.expectRevert();
-        lenderEnforcer.validate(new AdditionalTransfer[](0), loan, caveatData);
-
-        vm.expectRevert();
-        AstariaV1Lib.calculateCompoundInterest(AstariaV1Lib.MAX_DURATION, loan.debt[0].amount, MAX_RATE_PLUS_ONE, 18);
+    function testRateTooLowOneNonWADDecimal() public {
+        vm.expectRevert(AstariaV1Lib.InterestAccrualRoundingMinimum.selector);
+        AstariaV1Lib.validateCompoundInterest(1e18, 1, 10e17, 17);
     }
 
-    function testExceedMaxAmount() public {
-        defaultPricingData = abi.encode(BasePricing.Details({carryRate: 0, rate: 0, decimals: 18}));
-
-        BaseRecall.Details memory statusDetails = abi.decode(defaultStatusData, (BaseRecall.Details));
-        int256 exponent = int256((statusDetails.recallMax / 365 days) * AstariaV1Lib.MAX_DURATION);
-
-        uint256 MAX_AMOUNT_PLUS_ONE = type(uint256).max / uint256(exponent.expWad()) + 1;
-        Starport.Loan memory loan = generateDefaultLoanTerms();
-        loan.debt[0].amount = MAX_AMOUNT_PLUS_ONE;
-
-        bytes memory caveatData = _generateSignedCaveatLender(loan, lender, 0, true).caveats[0].data;
-        vm.expectRevert();
-        lenderEnforcer.validate(new AdditionalTransfer[](0), loan, caveatData);
-
-        vm.expectRevert();
-        AstariaV1Lib.calculateCompoundInterest(
-            AstariaV1Lib.MAX_DURATION, MAX_AMOUNT_PLUS_ONE, statusDetails.recallMax, 18
-        );
+    function testDecimalsTooLowZero() public {
+        vm.expectRevert(AstariaV1Lib.UnsupportedDecimalValue.selector);
+        AstariaV1Lib.validateCompoundInterest(1e18, 1, 10e18, 0);
     }
 
-    function testMaxRate() public {
-        uint256 MAX_RATE = ((((uint256(type(int256).max.lnWad())) / AstariaV1Lib.MAX_DURATION))) * 365 days;
-
-        defaultStatusData = abi.encode(
-            BaseRecall.Details({
-                honeymoon: 1 days,
-                recallWindow: 3 days,
-                recallStakeDuration: 30 days,
-                // 1000% APR
-                recallMax: MAX_RATE,
-                // 10%, 0.1
-                recallerRewardRatio: uint256(1e16) * 10
-            })
-        );
-        defaultPricingData = abi.encode(BasePricing.Details({carryRate: 0, rate: MAX_RATE, decimals: 18}));
-        Starport.Loan memory loan = generateDefaultLoanTerms();
-        loan.debt[0].amount = 2;
-
-        vm.expectRevert(abi.encodeWithSelector(AstariaV1Lib.InterestAccrualRoundingMinimum.selector));
-        lenderEnforcer.validate(new AdditionalTransfer[](0), loan, "");
-
-        AstariaV1Lib.calculateCompoundInterest(AstariaV1Lib.MAX_DURATION, loan.debt[0].amount, MAX_RATE, 18);
+    function testDecimalsTooHigh() public {
+        vm.expectRevert(AstariaV1Lib.UnsupportedDecimalValue.selector);
+        AstariaV1Lib.validateCompoundInterest(1e18, 1, 10e18, 19);
     }
 
-    function testMaxAmount() public {
-        defaultPricingData = abi.encode(BasePricing.Details({carryRate: 0, rate: 0, decimals: 18}));
-
-        BaseRecall.Details memory statusDetails = abi.decode(defaultStatusData, (BaseRecall.Details));
-        int256 exponent = int256((statusDetails.recallMax / 365 days) * AstariaV1Lib.MAX_DURATION);
-
-        uint256 MAX_AMOUNT = type(uint256).max / uint256(exponent.expWad());
-        Starport.Loan memory loan = generateDefaultLoanTerms();
-        loan.debt[0].amount = MAX_AMOUNT;
-        bytes memory caveatData = _generateSignedCaveatLender(loan, lender, 0, true).caveats[0].data;
-
-        vm.expectRevert(abi.encodeWithSelector(AstariaV1Lib.InterestAccrualRoundingMinimum.selector));
-        lenderEnforcer.validate(new AdditionalTransfer[](0), loan, "");
-
-        AstariaV1Lib.calculateCompoundInterest(AstariaV1Lib.MAX_DURATION, loan.debt[0].amount, 0, 18);
+    function testRateExceedsMaxRecallRate() public {
+        vm.expectRevert(AstariaV1Lib.RateExceedMaxRecallRate.selector);
+        AstariaV1Lib.validateCompoundInterest(1e18, 10e18 + 1, 10e18, 18);
     }
 
-    // function testPrecision() public {
-    //   uint256 result = AstariaV1Lib.calculateCompoundInterest(uint256(MAX_DURATION), MAX_AMOUNT, uint256(MAX_RATE)) + MAX_AMOUNT;
-    //   assertEq(result, MAX_UNSIGNED_INT, "Precision bounds not matching");
-    // }
+    function testMaxAmountDecimals() public {
+        uint256 i = 1;
+
+        for (; i < AstariaV1Lib.WAD + 1;) {
+            maxAmountDecimals(i);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function maxAmountDecimals(uint256 decimals) internal {
+        uint256 MAX_RATE = 10 ** (decimals + 1);
+        uint256 baseAdjustment = (10 ** (AstariaV1Lib.WAD - decimals));
+
+        int256 exponent = int256(((MAX_RATE * baseAdjustment * AstariaV1Lib.MAX_DURATION) / 365 days));
+
+        uint256 MAX_AMOUNT = (type(uint256).max / (uint256(exponent.expWad()) * baseAdjustment));
+
+        uint256 result =
+            AstariaV1Lib.calculateCompoundInterest(AstariaV1Lib.MAX_DURATION, MAX_AMOUNT, MAX_RATE, decimals);
+
+        vm.expectRevert(FixedPointMathLib.MulWadFailed.selector);
+        AstariaV1Lib.calculateCompoundInterest(AstariaV1Lib.MAX_DURATION, MAX_AMOUNT + 1, MAX_RATE, decimals);
+    }
+
+    function testInterestAccrual() public {
+        uint256 i = 1;
+
+        for (; i < AstariaV1Lib.WAD + 1;) {
+            interestAccrual(i);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function interestAccrual(uint256 decimals) public {
+        uint256 INTEREST = 1_718_281_828_459_045_235 / (10 ** (AstariaV1Lib.WAD - decimals));
+        uint256 AMOUNT = 10 ** decimals; // 1 in the provided decimal base
+        uint256 RATE = 10 ** decimals; // 100% in the provided decimal base
+
+        uint256 result = AstariaV1Lib.calculateCompoundInterest(365 days, AMOUNT, RATE, decimals);
+        assertEq(result, INTEREST, "Interest accrual calculation incorrect");
+    }
 }
