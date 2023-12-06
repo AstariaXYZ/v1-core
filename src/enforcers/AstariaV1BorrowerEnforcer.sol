3002fcdb179ba5985260a3d25169bbe64373965b
GregTheDev <mapler98@gmail.com>
2023-12-05 18:02:16 -0700

    revert block.number change

--- src/enforcers/AstariaV1BorrowerEnforcer.sol
+++ src/enforcers/AstariaV1BorrowerEnforcer.sol
@@ -33,8 +33,8 @@ contract AstariaV1BorrowerEnforcer is BorrowerEnforcer {
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     struct V1BorrowerDetails {
-        uint256 startBlock;
-        uint256 endBlock;
+        uint256 startTime;
+        uint256 endTime;
         uint256 startRate;
         uint256 maxAmount;
         uint256 minAmount;
@@ -109,22 +109,22 @@ contract AstariaV1BorrowerEnforcer is BorrowerEnforcer {
     function _locateCurrentRate(V1BorrowerDetails memory v1Details) internal view returns (uint256 currentRate) {
         uint256 endRate = AstariaV1Lib.getBasePricingRate(v1Details.details.loan.terms.pricingData);
 
-        // if endRate == startRate, or startBlock == endBlock, or block.number > endBlock
+        // if endRate == startRate, or startTime == endTime, or block.timestamp > endTime
         if (
-            endRate == v1Details.startRate || v1Details.startBlock == v1Details.endBlock
-                || block.number > v1Details.endBlock
+            endRate == v1Details.startRate || v1Details.startTime == v1Details.endTime
+                || block.timestamp > v1Details.endTime
         ) {
             return endRate;
         }
 
-        // Will revert if startBlock > endBlock
-        uint256 duration = v1Details.endBlock - v1Details.startBlock;
+        // Will revert if startTime > endTime
+        uint256 duration = v1Details.endTime - v1Details.startTime;
         uint256 elapsed;
         uint256 remaining;
         unchecked {
-            // block.number <= endBlock && startBlock < endBlock, can't overflow
-            elapsed = block.number - v1Details.startBlock;
-            // block.number <= endBlock, can't underflow
+            // block.timestamp <= endTime && startTime < endTime, can't overflow
+            elapsed = block.timestamp - v1Details.startTime;
+            // block.timestamp <= endTime, can't underflow
             remaining = duration - elapsed;
         }
 
