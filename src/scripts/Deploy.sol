a399999f13b3f8d997cc06eb505b68ef26b5a96a
Andrew Redden <opensource@andrewredden.com>
2023-12-07 18:14:23 -0400

    wip deployment and seed

--- src/scripts/Deploy.sol
+++ src/scripts/Deploy.sol
@@ -1,8 +1,8 @@
 pragma solidity =0.8.17;
-
+import {Vm} from "forge-std/Vm.sol";
 import {Script, console} from "forge-std/Script.sol";
 import {Starport, Stargate, CaveatEnforcer, AdditionalTransfer} from "starport-core/Starport.sol";
-import {TestERC20, TestERC721} from "starport-test/StarportTest.sol";
+import {TestERC20, TestERC721, TestERC1155} from "starport-test/StarportTest.sol";
 import {Consideration} from "seaport-core/src/lib/Consideration.sol";
 import {Custodian} from "starport-core/Custodian.sol";
 import {AstariaV1LenderEnforcer} from "src/enforcers/AstariaV1LenderEnforcer.sol";
@@ -21,21 +21,15 @@ contract Deploy is Script {
     Account public deployer;
     Account public liquidator;
 
-
     TestERC20 public erc20;
     TestERC721 public erc721;
+    TestERC1155 public erc1155;
 
-//    Starport public SP = Starport(address(0x75573d1dff9e2efe784d089f882a5ea320dd96f0));
+    //    Starport public SP = Starport(address(0x75573d1dff9e2efe784d089f882a5ea320dd96f0));
     Starport public SP;
     AstariaV1Pricing public v1Pricing;
     AstariaV1Status public v1Status;
     AstariaV1Settlement public v1Settlement;
-
-//    RacerOriginator public ftOriginator;
-//    RacerPricing public ftPricing;
-//    RacerHandler public fthandler;
-//    RacerMarginHook public ftMarginHook;
-//    RacerHelper public racerHelper;
 
     bool forkNet = false;
 
@@ -78,9 +72,11 @@ contract Deploy is Script {
         AstariaV1LenderEnforcer lenderEnforcer = new AstariaV1LenderEnforcer();
         erc20 = new TestERC20();
         erc721 = new TestERC721();
+        erc1155 = new TestERC1155();
         erc20.mint(address(lender.addr), 10e18);
         erc721.mint(address(borrower.addr), 1);
         erc721.mint(address(borrower.addr), 2);
+        erc1155.mint(address(borrower.addr), 1, 10);
         vm.stopBroadcast();
 
         vm.startBroadcast(borrower.key);
@@ -90,7 +86,7 @@ contract Deploy is Script {
         erc20.approve(address(SP), 10e18);
         vm.stopBroadcast();
 
-        seedOriginationData();
+//        seedOriginationData();
 
         string memory contracts = "contracts";
 
@@ -108,7 +104,6 @@ contract Deploy is Script {
         vm.writeJson(output, "./out/contract-addresses.json");
     }
 
-
     function seedOriginationData() internal {
         uint256 liquidatorKey = vm.envUint("LIQUIDATOR_PRIVATE_KEY");
         liquidator = Account({addr: vm.addr(liquidatorKey), key: liquidatorKey});
@@ -117,18 +112,47 @@ contract Deploy is Script {
 
         //non-liquidatable borrow
 
-        bytes memory pricingDetails = abi.encode(BasePricing.Details({
-            rate: 0.0000001 ether,
-            carryRate: 0.0000001 ether,
-            decimals: 18
-        }));
+        SpentItem[] memory collateral = new SpentItem[](1);
+        collateral[0] = SpentItem({itemType: ItemType.ERC721, token: address(erc721), identifier: 1, amount: 1});
+
+        //debt
+
+        SpentItem[] memory debt = new SpentItem[](1);
+        debt[0] = SpentItem({itemType: ItemType.ERC20, token: address(erc20), identifier: 0, amount: 1 ether});
 
         borrow(
-            pricingDetails
+        collateral,
+        debt,
+            abi.encode(BasePricing.Details({rate: 0.0000001 ether, carryRate: 0.0000001 ether, decimals: 18})),
+            abi.encode(
+                BaseRecall.Details({
+                    honeymoon: 7 days,
+                    recallWindow: 7 days,
+                    recallStakeDuration: 7 days,
+                    recallMax: 10e18,
+                    recallerRewardRatio: 0.5 ether
+                })
+            ),
+            abi.encode(DutchAuctionSettlement.Details({startingPrice: 100 ether, endingPrice: 100 wei, window: 7 days}))
         );
 
         //liquidatable borrow
-//        borrow(pricingDetails, 0x0, 0x1);
+        collateral[0] = SpentItem({itemType: ItemType.ERC721, token: address(erc721), identifier: 2, amount: 1});
+        recall(borrow(
+            collateral,
+            debt,
+            abi.encode(BasePricing.Details({rate: 0.0000001 ether, carryRate: 0.0000001 ether, decimals: 18})),
+            abi.encode(
+                BaseRecall.Details({
+                    honeymoon: 0,
+                    recallWindow: 0,
+                    recallStakeDuration: 0,
+                    recallMax: 10e18,
+                    recallerRewardRatio: 0.1 ether
+                })
+            ),
+            abi.encode(DutchAuctionSettlement.Details({startingPrice: 100 ether, endingPrice: 100 wei, window: 7 days}))
+        ));
     }
 
     struct FundingData {
@@ -142,13 +166,27 @@ contract Deploy is Script {
         vm.stopBroadcast();
     }
 
-    function borrow(bytes memory pricingData) internal {
+    function recall(Starport.Loan memory loan) internal {
+        vm.warp(block.timestamp + 1);
+
+        vm.startBroadcast();
+        try BaseRecall(loan.terms.status).recall(loan) {
+        } catch Error(string memory reason) {
+            loan.start--;
+            BaseRecall(loan.terms.status).recall(loan);
+        } catch (bytes memory reason) {
+            loan.start--;
+            BaseRecall(loan.terms.status).recall(loan);
+        }
+        vm.stopBroadcast();
+    }
+
+    function repay(Starport.Loan memory loan) internal {
+//        BaseRecall(loan.terms.status).repay(loan);
+    }
 
 
-        bytes memory statusData = abi.encode(BaseRecall.Details({honeymoon: 7 days, recallWindow: 7 days, recallStakeDuration: 7 days, recallMax: 10e18, recallerRewardRatio: 0.5 ether}));
-
-        bytes memory settlementData = abi.encode(DutchAuctionSettlement.Details({startingPrice: 100 ether, endingPrice: 100 wei, window: 7 days}));
-
+    function borrow(SpentItem[] memory collateral, SpentItem[] memory debt, bytes memory pricingData, bytes memory statusData, bytes memory settlementData) internal returns(Starport.Loan memory loan) {
         Starport.Terms memory terms = Starport.Terms({
             pricing: address(v1Pricing),
             pricingData: pricingData,
@@ -158,19 +196,6 @@ contract Deploy is Script {
             settlementData: settlementData
         });
 
-        SpentItem[] memory collateral = new SpentItem[](1);
-        collateral[0] = SpentItem({
-            itemType: ItemType.ERC721,
-            token: address(erc721),
-            identifier: 1,
-            amount: 1
-        });
-
-        //debt
-
-        SpentItem[] memory debt = new SpentItem[](1);
-        debt[0] = SpentItem({itemType: ItemType.ERC20, token: address(erc20), identifier: 0, amount: 1 ether});
-
         CaveatEnforcer.SignedCaveats memory caveatsB;
         //bool singleUse;
         //        uint256 deadline;
@@ -178,7 +203,7 @@ contract Deploy is Script {
         //        Caveat[] caveats;
         //        bytes signature;
         CaveatEnforcer.SignedCaveats memory caveats = CaveatEnforcer.SignedCaveats({
-            singleUse: true,
+            singleUse: false,
             deadline: block.timestamp + 100,
             salt: bytes32(0),
             caveats: new CaveatEnforcer.Caveat[](0),
@@ -192,11 +217,10 @@ contract Deploy is Script {
         //        CaveatEnforcer.Caveat[] calldata caveats
         //    )
 
-        bytes32 hash = SP.hashCaveatWithSaltAndNonce(lender.addr, true, 0, block.timestamp + 100, caveats.caveats);
+        bytes32 hash = SP.hashCaveatWithSaltAndNonce(lender.addr, caveats.singleUse, caveats.salt, caveats.deadline, caveats.caveats);
 
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(lender.key, hash);
         caveats.signature = abi.encodePacked(r, s, v);
-
 
         // uint256 start; // start of the loan
         //        address custodian; // where the collateral is being held
@@ -206,7 +230,7 @@ contract Deploy is Script {
         //        SpentItem[] collateral; // array of collateral
         //        SpentItem[] debt; // array of debt
         //        Terms terms; //
-        Starport.Loan memory loan = Starport.Loan({
+        loan = Starport.Loan({
             issuer: lender.addr,
             custodian: SP.defaultCustodian(),
             originator: lender.addr,
@@ -216,7 +240,19 @@ contract Deploy is Script {
             debt: debt,
             start: 0
         });
+
+        vm.recordLogs();
         vm.broadcast(borrower.key);
         SP.originate(new AdditionalTransfer[](0), caveatsB, caveats, loan);
+
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+
+        bytes32 lienOpenTopic = bytes32(0x57cb72d73c48fadf55428537f6c9efbe080ae111339b0c5af42d9027ed20ba17);
+        for (uint256 i = 0; i < logs.length; i++) {
+            if (logs[i].topics[0] == lienOpenTopic) {
+                (, loan) = abi.decode(logs[i].data, (uint256, Starport.Loan));
+                break;
+            }
+        }
     }
 }
